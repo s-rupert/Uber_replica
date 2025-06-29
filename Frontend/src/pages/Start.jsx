abf3bbbc9669de890b09c80fff72a75589431323
s-rupert <140841484+s-rupert@users.noreply.github.com>
2025-06-29 03:42:43 -0400

    Update Start.jsx

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Link } from "react-router-dom";
 
 const Start = () => {
+  useEffect(()=>{
+    if(window.innerWidth > 700){
+    alert("Your screen size is big, please try in phone size")
+    }
+  })
   return (
     <div>
       <div className="bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] h-screen pt-8 flex justify-between flex-col w-full bg-red-200">
